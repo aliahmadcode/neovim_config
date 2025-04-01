f835b26080bee707c25389de87ec6a703241c525
Ali Ahmad <aliahmadgujjar895@gmail.com>
2025-04-01 11:15:36 +0500

    GIT

--- lua/ali/lazy/formatter.lua
+++ lua/ali/lazy/formatter.lua
@@ -1,14 +1,3 @@
 return {
   'nvimtools/none-ls.nvim',
-  config = function()
-    local none_ls = require('none-ls')
-
-    none_ls.setup({
-      sources = {
-        none_ls.builtins.formatting.prettier.with({
-          extra_args = { "--tab-width", "2" }
-        })
-      },
-    })
-  end,
 }
