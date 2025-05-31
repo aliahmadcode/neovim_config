402fd979db8ca8a98622bab48d585e12817658d5
Ali Ahmad <aliahmadgujjar895@gmail.com>
2025-05-31 11:19:25 +0500

    windsurf

--- lua/ali/lazy/ai.lua
+++ lua/ali/lazy/ai.lua
@@ -7,7 +7,7 @@ return {
 
   config = function()
     require("codeium").setup({
-      enable_cmp_source = false,
+      enable_cmp_source = true,
       virtual_text = {
         enabled = true,
         manual = false,
