71033e573bec059b76d11685eac2fda6674a988f
Ali Ahmad <aliahmadgujjar895@gmail.com>
2025-06-21 14:33:50 +0500

    123

--- lua/ali/lazy/terminal.lua
+++ lua/ali/lazy/terminal.lua
@@ -1,13 +1,12 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-    config = function()
-        local toggleterm = require("toggleterm")
-        toggleterm.setup({
-            open_mapping = [[<C-\>]],
-            insert_mappings = true,
-            terminal_mappings = true,
-        })
-    end,
+  config = function()
+    local toggleterm = require("toggleterm")
+    toggleterm.setup({
+      open_mapping = [[<C-\>]],
+      insert_mappings = true,
+      terminal_mappings = true,
+    })
+  end,
 }
-
